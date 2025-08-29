f5edb06240ec05765b7ac6fac824746ebaa388f2
jiezheng5 <jiezheng5@gmail.com>
2025-08-28 19:52:10 -0700

    Remove docs, infrastructure folders from remote repo only

--- infrastructure/bin/infrastructure.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { InfrastructureStack } from '../lib/infrastructure-stack';
-
-const app = new cdk.App();
-new InfrastructureStack(app, 'time-tracking-app', {
-  /* Using the AWS Account and Region from the current CLI configuration */
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
-});
